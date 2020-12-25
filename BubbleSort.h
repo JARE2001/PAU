82272cf5a11f9c9988cb14e8152c152345a596a2
pau-cardoso <pauc@LAPTOP-C5TRASB4.localdomain>
2020-12-25 15:26:19 -0600

    este es el bueno

--- BubbleSort.h
+++ BubbleSort.h
@@ -1,18 +1,5 @@
-#ifndef BUBBLE_SORT_H_
-#define BUBBLE_SORT_H_
-
 #include <vector>
-#include <list>
-
 using namespace std;
-
-template <class T>
-class BubbleSort {
-    private:
-        void swap( vector<T> &, int, int ); // Funcion para intercambiar posiciones
-    public:
-        void bubbleSort( vector<T> & ); // Algoritmo de ordenamiento de burbuja
-};
 
 /*
     Funcion para intercambiar la posicion de dos elementos
@@ -21,11 +8,12 @@ class BubbleSort {
     @param j indice del otro elemento a intercambiar
 */
 template <class T>
-void BubbleSort<T>::swap( vector<T> &v, int i, int j ){
+void swap( vector<T> &v, int i, int j ){
     T temp = v[i];
     v[i] = v[j];
     v[j] = v[temp];
 }
+
 
 /*
     Ordenamiento de burbuja
@@ -33,19 +21,17 @@ void BubbleSort<T>::swap( vector<T> &v, int i, int j ){
     @return Nada pero modifica el vector y lo ordena
 */
 template <class T>
-void BubbleSort<T>::bubbleSort( vector<T> &v )
+void bubbleSort( vector<T> &v )
 {
     for ( int i = v.size() - 1; i > 0; i--)
     {
         for (int j = 0; j < i; j++)
         {
             if ( v[j] > v[j+1] ){
-                swap(v, j, j+1)
+                swap(v, j, j + 1);
             }
             
         }
     }
     return;
 }
-
-#endif /* BUBBLE_SORT_H_ */
