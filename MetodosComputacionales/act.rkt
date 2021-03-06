c3f5a053ea2bc504ae763c5cfee2eb516f7658ba
JR-espacial <jorge_alan_2001@hotmail.com>
2021-03-05 21:33:31 -0600

    Act2.1 hasta el 12

---
+++ MetodosComputacionales/act.rkt
@@ -0,0 +1,73 @@
+#lang racket
+
+;;(enter! (file "act.rkt"))
+
+;;Ejercicio 1 Farenheit Celcius
+
+(define (f-c f)
+  (/ (* 5 (- f 32)) 9))
+
+;;Sign
+
+(define(sign a)
+  (cond
+    [(= a 0) 0]
+    [(< a 0) -1]
+    [else 1]))
+;;Roots
+
+(define(roots a b c)
+  (/ (+ (- b)(sqrt (- (* b b) (* 4 a c))))(* 2 a)))
+
+;;BMI
+(define (print_bmi b)
+  (cond
+    [(< b 20) (string #\u #\n #\d #\e #\r #\w #\e #\i #\g #\h #\t)]
+    [(and (>= b 20) (< b 25))(string #\n #\o #\r #\m #\a #\l)]
+    [(and (>= b 25) (< b 30))(string #\o #\b #\e #\s #\e #\1)]
+    [(and (<= 30 b) (< b 40))(string #\o #\b #\e #\s #\e #\2)]
+    [else (string #\o #\b #\e #\s #\e #\3)]))
+
+(define (bmi w h)
+  (print_bmi
+    (/ w (* h h))))
+
+;;Factorial
+(define (factorial n)
+    (cond
+      [(< n 1) 1]
+      [else (* n (factorial (- n 1)))]))
+
+;;Duplicate
+(define (duplicate lst)
+  (cond
+    [(empty? lst) '() ]
+    [else (append (list (car lst) (car lst)) (duplicate (cdr lst)))]))
+
+;;POW
+
+(define (pow a b)
+  (cond
+    [(eq? b 0) 1]
+    [else (* a (pow a (- b 1)))]))
+
+;;FIB
+(define (fib n)
+    (cond
+      [(< n 3) 1]
+      [else (+ (fib (- n 1)) (fib (- n 2)))]))
+;;Enlist
+(define (enlist lst)
+  (cond
+    [(empty? lst) '() ]
+    [else (cons (list (car lst)) (enlist (cdr lst)))]))
+;;Add-list
+(define (add-list lst)
+  (cond
+    [(empty? lst) 0 ]
+    [else (+ (car lst) (add-list (cdr lst)))]))
+;;Invert Pairs
+(define (invert-pairs lst)
+  (cond
+    [(empty? lst) '()]
+    [else (cons (append (cdr (car lst)) (list(car(car lst))) )(invert-pairs (cdr lst)) )]))
